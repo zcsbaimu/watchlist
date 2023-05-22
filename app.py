d227c9888794c42685ad977e0434082b4e05db12
baimu <3276605184@qq.com>
2023-05-22 17:26:12 +0800

    ff

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app=Flask(__name__)
 @app.route('/')
+@app.route('/index')
+@app.route('/home')
 def hello():
-   return "Welcome to My Watchlist!"
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
