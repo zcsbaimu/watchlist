9a9e0174a430901f819e8d1d7f6d69e4a2d5caac
baimu <3276605184@qq.com>
2023-05-22 19:48:38 +0800

    s

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
-s = '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-
-
 @app.route('/user/<name>')
 def user_name(name):
     return f'User: {escape(name)}'
 @app.route('/')
 def hello():
-    return 'Hello'
-
+    return 's'
+@app.route('/gg')
+def hellso():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 @app.route('/test')
 def test_url_for():
     # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
@@ -23,3 +22,5 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+if __name__ == "__main__":
+    app.run(debug=True)
