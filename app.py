0e6aa51d8940d14d53c4fb240144294daf86ef59
baimu <3276605184@qq.com>
2023-05-23 17:46:11 +0800

    add database support with flask-sqlalchemy

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
 from flask import Flask, url_for
 from flask import render_template
-
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+import click
 app = Flask(__name__)
-@app.route('/s')
-def index():
-    return render_template('index.html',name=name,movies=movies)
-@app.route('/')
-def hellso():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-name="baimulong"
-movies = [
+
+@app.route('/')
+def index():
+    user=User.query.first()
+    movies=Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
+
+
+@app.route('/s')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+@app.cli.command()
+@click.option('--drop', is_flag=True,help='Create afetr drop')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialiezd database')
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = "baimulong"
+    movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
     {'title': 'A Perfect World', 'year': '1993'},
@@ -20,7 +41,25 @@ movies = [
     {'title': 'King of Comedy', 'year': '1999'},
     {'title': 'Devils on the Doorstep', 'year': '1999'},
     {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+    {'title': 'The Pork of Music', 'year': '2012'},]
+    user=User(name=name)
+    for m in movies:
+        movie=Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('done')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+        app.run(debug=True)
