e9a747c29942984ff472bcf358f1d16431770c12
baimu <3276605184@qq.com>
2023-05-25 11:39:28 +0800

    s

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask, url_for, request, flash, redirect
-from flask import render_template
+from flask import Flask, url_for, request, flash, redirect,render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 import click
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin,login_user,LoginManager,logout_user,login_required,current_user
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dev'  # app.secret_key='dev'
 
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message='please login'
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return  redirect(url_for('index'))
         title = request.form.get('title')
         year = request.form.get('year')
         if not title or not year or len(year) > 4 or len(title) > 60:
@@ -27,9 +36,43 @@ def index():
         return redirect(url_for('index'))
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
-
+@app.route('/settings',methods=['GET','POST'])
+@login_required
+def settings():
+    if request.method=="POST":
+        name=request.form['name']
+        if not name or len(name)>20:
+            flash('Invalid input.')
+            return  redirect(url_for('settings'))
+        current_user.name=name
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('settings.html')
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        if not username or not password:
+            flash('INvalid input')
+            return  redirect(url_for('login'))
+        user=User.query.first()
+        if username==user.username and user.verify_password(password):
+            login_user(user)
+            flash('login success')
+            return redirect(url_for('index'))
+        flash('Invalid username or password')
+        return  redirect(url_for('index'))
+    return render_template('login.html')
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Goodbye')
+    return redirect(url_for('index'))
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     if request.method == 'POST':
@@ -45,13 +88,17 @@ def edit(movie_id):
         return redirect(url_for('index'))  # 重定向回主页
 
     return render_template('edit.html', movie=movie)
-@app.route('/movie/delete/<int:movie_id>',methods=['POST'])
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
-    movie=Movie.query.get_or_404(movie_id)
+    movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
     db.session.commit()
     flash(('item deleted.'))
     return redirect(url_for('index'))
+
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create afetr drop')
@@ -60,6 +107,26 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialiezd database')
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='the username used to login')
+@click.option('--password', prompt=True, confirmation_prompt=True, help='The password used to login')
+def admin(username, password):
+    '''Create user'''
+    db.create_all()
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+    db.session.commit()
+    click.echo('Done')
 
 
 @app.cli.command()
@@ -97,9 +164,17 @@ def inject_user():
     return dict(user=user)
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(120))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Movie(db.Model):
--- sd.py
+++ sd.py
@@ -1,19 +1,4 @@
-import time
-
-def fathers(func):
-    def gg(*args, **kargs):
-        print('sdsds')
-        f=func(*args,**kargs)
-        return f
-    return gg
-@fathers
-def add(a,b):
-    return  a+b
-# def time_calc(func):
-#     def wrapper(*args, **kargs):
-#         start_time = time.time()
-#         f = func(*args,**kargs)
-#         exec_time = time.time() - start_time
-#         return f
-#     return wrapper
-add(3,2)
+from werkzeug.security import  generate_password_hash,check_password_hash
+pw_hash=generate_password_hash('dog')
+print(pw_hash)
+print(check_password_hash(pw_hash,'dog'))
